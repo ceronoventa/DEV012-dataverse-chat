ed77cfd4a2987dab2d4e015772e615b3c7ee8d4b
belh osorio <belh.osoriocom@gmail.com>
2023-11-30 18:48:18 -0300

    avance del root e indexjs

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
-// En este archivo definirás tus rutas e importarás los componentes que vas a renderizar.
+import { setRoutes, setRootElement, onURLChange } from './route.js';
 
-/*
-import Example from './views/Example.js';
-
-Ejemplo de definición de rutas:
-
-const routes = {
-    "/": Example,
-    ...
-}
-*/
-
-/*
-TODO:
-1.- Definir rutas en router.
-2.- Pasar "root element" a router.
-3.- Invocar el router para renderizar la vista correcta.
-*/
-import {renderView} from './router.js';
-
-import {apiKey} from './views/apiKey.js';
 import {error} from './views/error.js';
 import {home} from './views/home.js';
-import {panel} from './views/panel.js';
-import {singleChat} from './views/singleChat.js';
+// import {apiKey} from './views/apiKey.js';
+// import {panel} from './views/panel.js';
+// import {singleChat} from './views/singleChat.js';
 
 const routes = {
     "/": home,
-    "/singleChat": singleChat,
-    "/panel": panel,
-    "/apikey": apiKey,
     "/error": error,
+    //"/singleChat": singleChat,
+    // "/panel": panel,
+    // "/apikey": apiKey,*/
   
   };
+
+setRoutes(routes);
+setRootElement(document.getElementById('root'));
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log(event.target.location.pathname)
+onURLChange(event.target.location.pathname)
+});
--- src/router.js
+++ src/router.js
@@ -1,26 +1,30 @@
 let ROUTES = {};
 let rootElement;
 
-export const setRootEl = (el) => {
+export const setRootElement = (newRootElementValue) => {
   // assign rootEl
+  rootElement = newRootElementValue;
 }
 
-export const setRoutes = (routes) => {
+export const setRoutes = (newRoutesValue) => {
   // optional Throw errors if routes isn't an object
   // optional Throw errors if routes doesn't define an /error route
   // assign ROUTES
-}
+  if(typeof newRoutesValue === Object){
+    if(newRoutesValue['/error']){ 
+  ROUTES = newRoutesValue;
+}}};
 
 const renderView = (pathname, props={}) => {
   // clear the root element
   const root = rootElement;
-  root.innerHTML ="";
+  root.innerHTML ='';
   // find the correct view in ROUTES for the pathname
   if(ROUTES[pathname]){
-    const template = ROUTES[pathname]();
+    const template = ROUTES[pathname](props);
     root.appenChild(template);
   } else{
-    root.appenChild(ROUTES["/error"]());
+    root.appenChild(ROUTES['/error'](props));
   }
   
   // in case not found render the error view
@@ -30,11 +34,15 @@ const renderView = (pathname, props={}) => {
 
 export const navigateTo = (pathname, props={}) => {
   // update window history with pushState
+  const URLvisited = window.location.hostname + pathname;
+  history.pushState({}, "", URLvisited);
   // render the view with the pathname and props
+  renderView(pathname, props);
 }
 
 export const onURLChange = (location) => {
   // parse the location for the pathname and search params
   // convert the search params to an object
   // render the view with the pathname and object
+  renderView(location);
 }
