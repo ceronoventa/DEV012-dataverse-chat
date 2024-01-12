68059818e784ce8bcd556d49872137b05bcdadda
belh osorio <belh.osoriocom@gmail.com>
2024-01-12 19:25:59 -0300

    chatgrupal listo

--- src/lib/openIaAPI.js
+++ src/lib/openIaAPI.js
@@ -1,4 +1,9 @@
 export const chatCompletions = (KEY, data) => {
+
+// const bodyRequest ={
+//     model: "gpt-3.5-turbo-1106",
+//     messages: data,
+// }
  
 return fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
--- src/test/openIaAPI.spec.js
+++ src/test/openIaAPI.spec.js
@@ -1,6 +1,6 @@
 import { chatCompletions } from "../lib/openIaAPI.js";
 
-const OpenIAResponse = jest.fn().mockResolvedValueOnce({choice:[{message :'foo'}]});
+const OpenIAResponse = jest.fn();
 
 global.fetch = jest.fn(() => Promise.resolve({
     json: OpenIAResponse
@@ -9,22 +9,46 @@ global.fetch = jest.fn(() => Promise.resolve({
 
 describe('Endpoint de openIA', () => {
     it ('La API es llamada con los datos adecuados'), () =>{
-     const messages =[{
-        role: "user",
-        content: "foo",
+     
+     OpenIAResponse.mockResolvedValueOnce({choice:[{message :'foo'}]});
+        const body =[{
+            model: "gpt-3.5-turbo-1106",
+            messages:{
+            role: "user",
+        content: "foo"}
       }]; 
-      chatCompletions = ("1234567", messages);
+     
+      
+      chatCompletions ("1234567", body);
       expect (global.fetch).toBeCallendWith("https://api.openai.com/v1/chat/completions", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer 1234567`,
             },
-                body: JSON.stringify({
-                model: "gpt-3.5-turbo-1106",
-                messages,
-                
-        })
+                body: JSON.stringify(body)
     });
-        
+} 
+
+it ('El edpoint responde de manera correcta', () => {
+    const response ={
+        "choise": [
+            {
+                "message":{
+                    "role": "assistant",
+                    "content": "¡Hola!"
+                }
+            }]
+    };
+   
+OpenIAResponse.mockResolvedValueOnce(response);
+return chatCompletions("1234567", [{
+    role: "user",
+    content: "foo",
+  }]).then ((resolved) =>{
+    expect(resolved).toBe(response);
+  });
+});
+
+
 });
