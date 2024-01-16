209665aac13385f4d807e9e776d57d5a717af2a0
Camila Palma <cpalma.contacto@gmail.com>
2024-01-16 16:15:09 -0300

    prototiposyreadme

--- src/lib/dataFunctions.js
+++ src/lib/dataFunctions.js
@@ -2,61 +2,60 @@
 
 // una función filterBy que tiene 3 parámetros (data, filterBy, value) y devuelve el arreglo filtrado
 export const searchByName = (data, searchBy, input) => {
-    if (searchBy === "input") {
-      const filteredDataByName = data.filter((item) => {
-        return item.name.toLowerCase().includes(input.toLowerCase());
-      });
-      return filteredDataByName;
-    }
-  };
-  
-  // una función filterBy que tiene 3 parámetros (data, filterBy, value) y devuelve el arreglo filtrado
-  export const filterByGenre = (data, filterBy, value) => {
-    return data.filter((item) => item.facts[filterBy].includes(value));
-  };
-  
-  export const filterByStudio = (data, filterBy, value) => {
-    return data.filter((item) => item.facts[filterBy].includes(value));
-  };
-  
-  export const filterByYear = (data, filterBy, value) => {
-    if (data && !isNaN(value)) {
-      return data.filter((item) => item.facts[filterBy] === Number(value));
-    }
-  };
-  
-  //una función sortBy que tiene 3 parámetros (data, sortBy, sortOrder) y devuelve el arreglo ordenado
-  export const sortData = (data, sortBy, sortOrder) => {
-    const copyData = [...data];
-    copyData.sort((a, b) => {
-      const valueA = a[sortBy];
-      const valueB = b[sortBy];
-  
-      if (valueA === valueB) {
-        return 0;
-      }
-  
-      if (sortOrder === "asc") {
-        if (valueA < valueB) {
-          return -1;
-        }
-        return 1;
-      }
-  
-      if (sortOrder === "desc") {
-        if (valueB < valueA) {
-          return -1;
-        }
-        return 1;
-      }
+  if (searchBy === "input") {
+    const filteredDataByName = data.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLowerCase());
     });
-  
-    return copyData;
-  };
-  
-  //una función computeStats que tiene al menos un parámetro (data) y devuelve un valor computado
-  export const computeStats = (data) => {
-    const longitud = data.reduce((acumulador) => acumulador + 1, 0);
-    return longitud;
-  };
-  
+    return filteredDataByName;
+  }
+};
+
+// una función filterBy que tiene 3 parámetros (data, filterBy, value) y devuelve el arreglo filtrado
+export const filterByGenre = (data, filterBy, value) => {
+  return data.filter((item) => item.facts[filterBy].includes(value));
+};
+
+export const filterByStudio = (data, filterBy, value) => {
+  return data.filter((item) => item.facts[filterBy].includes(value));
+};
+
+export const filterByYear = (data, filterBy, value) => {
+  if (data && !isNaN(value)) {
+    return data.filter((item) => item.facts[filterBy] === Number(value));
+  }
+};
+
+//una función sortBy que tiene 3 parámetros (data, sortBy, sortOrder) y devuelve el arreglo ordenado
+export const sortData = (data, sortBy, sortOrder) => {
+  const copyData = [...data];
+  copyData.sort((a, b) => {
+    const valueA = a[sortBy];
+    const valueB = b[sortBy];
+
+    if (valueA === valueB) {
+      return 0;
+    }
+
+    if (sortOrder === "asc") {
+      if (valueA < valueB) {
+        return -1;
+      }
+      return 1;
+    }
+
+    if (sortOrder === "desc") {
+      if (valueB < valueA) {
+        return -1;
+      }
+      return 1;
+    }
+  });
+
+  return copyData;
+};
+
+//una función computeStats que tiene al menos un parámetro (data) y devuelve un valor computado
+export const computeStats = (data) => {
+  const longitud = data.reduce((acumulador) => acumulador + 1, 0);
+  return longitud;
+};
--- src/lib/openIaAPI.js
+++ src/lib/openIaAPI.js
@@ -1,19 +1,17 @@
 export const chatCompletions = (KEY, data) => {
+  // const bodyRequest ={
+  //     model: "gpt-3.5-turbo-1106",
+  //     messages: data,
+  // }
 
-// const bodyRequest ={
-//     model: "gpt-3.5-turbo-1106",
-//     messages: data,
-// }
- 
-return fetch("https://api.openai.com/v1/chat/completions", {
+  return fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${KEY}`,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${KEY}`,
     },
-        body: JSON.stringify(data)
-})
-.then((response) => {
-return response.json();
-})
-}
+    body: JSON.stringify(data),
+  }).then((response) => {
+    return response.json();
+  });
+};
